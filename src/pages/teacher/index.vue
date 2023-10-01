c311a5556e5dd7f91c6cbe25bf1f8a8ca6294fc9
kui <872505550@qq.com>
2023-10-01 12:03:06 +0800

    feat: teacher

--- src/pages/teacher/index.vue
+++ src/pages/teacher/index.vue
@@ -30,14 +30,17 @@ const list = [
     <SwiperPage>
       <swiper-slide>
         <div class="page1-wrap">
-          <div class="left-img-text-wrap">
-            <a-image :src="getImageUrl('teacher/list1.png')" alt="" />
-            <p>畜禽健康养殖教师团队合影</p>
-          </div>
-          <a-image :src="getImageUrl('teacher/list2.png')" alt="" />
+          <h3>全国高校黄大年式教师团队</h3>
           <p class="label">
             华中农业大学畜禽健康养殖教师团队由家畜传染病学家陈焕春院士于上世纪90年代初组建，30多年来，团队坚持“以立德树人为根本，以强农兴农为己任”，聚焦畜禽健康养殖关键问题，深化科教产教融合，薪火传承、接续奋斗，书写了“人才立国、科技强国、产业兴国”的华农牧医华章，为我国农牧高等教育事业和畜牧业现代化建设作出了重要贡献。
           </p>
+          <div class="img-list">
+            <div class="left-img-text-wrap">
+              <a-image :src="getImageUrl('teacher/list1.png')" alt="" />
+              <p>畜禽健康养殖教师团队合影</p>
+            </div>
+            <a-image :src="getImageUrl('teacher/list2.png')" alt="" />
+          </div>
         </div>
       </swiper-slide>
       <swiper-slide>
@@ -74,21 +77,28 @@ const list = [
     }
   }
   .page1-wrap {
-    margin-top: 31px;
-    .flex-row;
-    align-items: flex-start;
+    margin-top: 20px;
+    h3 {
+      font-size: 32px;
+      text-align: center;
+    }
     p {
       font-size: 24px;
       font-weight: 500;
       color: #000;
     }
-    .left-img-text-wrap {
-      .flex-col;
-      gap: 33px;
-      :deep(.ant-image) {
-        width: 896px;
+    .img-list {
+      .flex-row;
+      align-items: stretch;
+      .left-img-text-wrap {
+        .flex-col;
+        gap: 33px;
+        :deep(.ant-image) {
+          width: 896px;
+        }
       }
     }
+
     :deep(.ant-image) {
       width: 399px;
       height: 522px;
